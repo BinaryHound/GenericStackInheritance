a5731c028c7b8718c33cdb8c6dece70e343eed9b
lennardneuwirth <lennardneuwirth@192.168.1.21>
2017-10-01 21:46:22 -0600

    Modified code with some more comments/explanations.

--- src/genericstackinheritance/TestClass.java
+++ src/genericstackinheritance/TestClass.java
@@ -7,17 +7,26 @@ package genericstackinheritance;
 public class TestClass {
     
     public static void main(String[] args){
+        
+        //simple data holders.
         char[] chars;
         String s = "";
+        
+        //Instantiating a new instance of the ArrayList stack.
         GenericStackInheritance<String> genString = new GenericStackInheritance<>();
         
+        //Adding some strings to the list.
         genString.Push("abc");
         genString.Push("def");
         genString.Push("ghi");
         genString.Push("jkl");
         genString.Push("mno");
+        
+        //Checking to see if the stack works.
         System.out.println(genString.getSize());
         System.out.println(genString.toString());
+        
+        //Takes the stack and makes all of the strings return backwards for fun.
         for(int i = 0; genString.getSize() > 0; i++){
             s = "";
             chars = genString.Pop().toCharArray();
