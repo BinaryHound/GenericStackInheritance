2d03b865a6e98b21d029d8abd8097017af02eb73
lennardneuwirth <lennardneuwirth@192.168.1.21>
2017-10-01 21:43:14 -0600

    Modified code with some more comments.

--- src/genericstackinheritance/GenericStackInheritance.java
+++ src/genericstackinheritance/GenericStackInheritance.java
@@ -9,26 +9,38 @@ import java.util.ArrayList;
 
 public class GenericStackInheritance<E> extends ArrayList<E> {
 
+    //Instantiate a new arrayList.
     ArrayList<E> list = new ArrayList<>();
     
+    //Take an object as a parameter and add it to the list.
     public void Push(E o){
         list.add(o);
     }
+    
+    //Look at the first item of the list.
     public E Peek(){
         return list.get(list.size() - 1);
     }
+    
+    //Return the first item of the list, and remove that item from the list.
     public E Pop(){
         E o = list.get(list.size() - 1);
         list.remove(list.size() - 1);
         return o;
     }
+    
+    //Get the size of the list.
     public int getSize(){
         return list.size();
     }
+    
+    //Override the ArrayList is empty to call this version.
     @Override
     public boolean isEmpty(){
         return list.isEmpty();
     }
+    
+    //Override the toString call to make it more accurate for this list.
     @Override
     public String toString(){
         return "Stack: " + list.toString();
